08d67b660e8964e2fcae97cc019bb5094126797e
junioralcant <junioralcant111@gmail.com>
2023-01-23 10:03:35 -0300

    refactor: update service api and change version app

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,8 +2,8 @@ import axios, {AxiosRequestConfig} from 'axios';
 import AsyncStorage from '@react-native-community/async-storage';
 
 const api = axios.create({
-  // baseURL: 'https://api-spp-api.herokuapp.com',
-  baseURL: 'http://192.168.0.107:3001',
+  baseURL: 'https://api-spp-api.herokuapp.com',
+  // baseURL: 'http://192.168.0.107:3001',
 });
 
 api.interceptors.request.use(
